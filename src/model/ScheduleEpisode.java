4954c8cafddcb73015d680c082982f3b9bface61
id16afk <id16afk@cs.umu.se>
2019-01-01 19:38:06 +0100

    started on the schedule

---
+++ src/model/ScheduleEpisode.java
@@ -0,0 +1,14 @@
+package model;
+
+public class ScheduleEpisode {
+    private String title;
+    private String description;
+    private String startTime;
+    private String endTime;
+    private int programID;
+    private String programName;
+    private int channelID;
+
+
+
+}
